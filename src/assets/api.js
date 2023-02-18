ecf38846b03edeab304694ec13580f5000661ee6
Pedro Souza <108759317+PedroSzSantana@users.noreply.github.com>
2023-02-18 00:24:20 -0300

    Update api.js

--- src/assets/api.js
+++ src/assets/api.js
@@ -1,12 +1,11 @@
 import axios from "axios"
 
 export const getInfo = async (state, city) => {
-    axios.get(`http://api.weatherapi.com/v1/current.json?key=98962b2e751c4791a7513515230401&q=${city}&lang=pt`)
-        .then((response) => {
-            state(response.data)
-            console.log(response)
-
-        }).catch((error) => {
+   const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=98962b2e751c4791a7513515230401&q=${city}&lang=pt`)
+   
+      state(response.data)
+    
+        .catch((error) => {
             console.log(error)
         })
 }
